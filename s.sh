bd18c82bfe234d35258ebf4117112de950b7eeb2
creslinux <creslin@gmail.com>
2023-06-08 13:25:03 +0530

    remove no product directories

--- s.sh
+++ s.sh
@@ -1,6 +1,6 @@
 rm static/*.html
-pygmentize -f html -O style=monokai -O full -o static/first_version_1_be_src.html be.py
-pygmentize -f html -O style=monokai -O full -o static/refactor_app_src.html be_app.py
-pygmentize -f html -O style=monokai -O full -o static/refactor_platform_controller.html platform_controller/platform_controller.py
-pygmentize -f html -O style=monokai -O full -o static/refactor_app_usecases_src.html platform_usecases/app_usecases.py
-pygmentize -f html -O style=monokai -O full -o static/refactor_models_src.html platform_models/models.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/first_version_1_be_src.html be.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_app_src.html be_app.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_platform_controller.html platform_controller/platform_controller.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_app_usecases_src.html platform_usecases/app_usecases.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_models_src.html platform_models/models.py
