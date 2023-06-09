c6e01db80d435a00495bf364bdc8ba42844a97e9
creslinux <creslin@gmail.com>
2023-06-09 09:54:02 +0530

    add automation gh actionst to src explorer

--- s.sh
+++ s.sh
@@ -4,3 +4,4 @@ venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_app_src.
 venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_platform_controller.html platform_controller/platform_controller.py
 venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_app_usecases_src.html platform_usecases/app_usecases.py
 venv/bin/pygmentize -f html -O style=monokai -O full -o static/refactor_models_src.html platform_models/models.py
+venv/bin/pygmentize -f html -O style=monokai -O full -o static/github_actions_pipeline.html .github/workflows/lxc-ec2.yml
